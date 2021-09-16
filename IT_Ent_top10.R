a30441ea1b2bef06c3c49046e52950ad4e9cab84
DongGyu Jung <86935274+yummygyudon@users.noreply.github.com>
2021-09-16 11:43:41 +0900

    ITEnt CSV파일

--- IT_Ent_top10.R
+++ IT_Ent_top10.R
@@ -7,4 +7,6 @@ KR_Ent <- read.csv("KR_Enterpise.csv", encoding = "utf-8")
 View(KR_Ent)
 sec <- c("IT.전자", "은행.지주", "정보통신 외", "엔지니어링")
 
-IT_Ent <- KR_Ent %>% filter(산업 %in% sec) %>%  select(이름) %>% head(10) 
+IT_Ent <- KR_Ent %>% filter(산업 %in% sec) %>%  select(이름) 
+
+write.csv(IT_Ent, "IT_Ent.csv")
